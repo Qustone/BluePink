937e55c963135b4552fc094996fee4b7cfa48123
Qustone <alexkhr92@yandex.ru>
2019-02-05 13:42:22 +0300

    Qustone

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>BluePink</title>
+</head>
+<body>
+    <div class="main">
+
+		<!--Шапка-->
+<header>
+    <div id ="logo">
+        <a href="#">
+            <span style="color: #373e47">Blue</span><span style="color: #d380be">Pink</span><span style="color: #8b95a4">.com</span>
+        </a>
+    </div>
+
+    <div class="navigation">
+            <a href="#">Главная</a>
+            <a href="#">Видео</a>
+            <a href="#">Новости</a>
+            <a href="#">Контакты</a>
+    </div>
+
+</header>
+
+		<!--Основная часть/ Левое меню-->
+<main>
+    <div class="left_menu">
+        <ul class="left_one">
+            <li><a href="#">Ссылка-1</a></li>
+            <li><a href="#">Ссылка-2</a></li>
+            <li><a href="#">Ссылка-3</a></li>
+            <li><a href="#">Ссылка-4</a></li>
+            <li><a href="#">Ссылка-5</a></li>
+        </ul>
+
+        <div class="left_two">
+            <p><span class="title-block">Left Block</span><br /><br />
+                Vivamus sagittis vestibulum nisi, a accumsan nisi lacinia nec. 
+                Proin venenatis sagittis felis at tincidunt. Morbi sollicitudin, 
+                nunc eu tincidunt sagittis, lacus metus pellentesque nibh, in 
+                ullamcorper augue tortor sit amet libero. Aenean aliquam, felis 
+                ut fermentum sodales, nisi mauris pharetra sem, a vehicula 
+                ligula diam sit amet ligula. Donec aliquam iaculis diam 
+                pellentesque fringilla.
+            </p>
+            <a href="#">Read more</a>
+        </div>
+    </div>
+
+		<!-- Правое меню -->
+    <div class="right_menu">
+
+            <!-- Картинки -->
+        <div class="right_two">
+            <h3>Картинки</h3>
+            <div class="img">
+                <img src="images/1.jpg" alt="img1">
+                <img src="images/2.jpg" alt="img2">
+                <img src="images/3.jpg" alt="img3">
+            </div>
+        </div>
+    </div>
+</main>
+
+		<!--Подвал-->
+<footer>
+    <div class="footer">© Подвал <a href="#">Link footer</a>
+    </div>
+</footer>
+
+</div>
+</body>
+</html>
